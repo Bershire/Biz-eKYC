cd4c765cee944c830dfdfd4f4406ebb7faa48747
thanhnt <nguyentienthanh.work@gmail.com>
2026-01-12 11:59:25 +0700

    fix build

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,24 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withSentryConfig } = require('@sentry/react-native/metro');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+const config = {
+  projectRoot: __dirname,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, 'bin'].filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+};
+
+module.exports = withSentryConfig(mergeConfig(defaultConfig, config), {
+  annotateReactComponents: true,
+});
